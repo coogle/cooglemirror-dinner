8c10e90154bde41f5eb7a8e10d2a842282751e28
John Coggeshall <john@coggeshall.org>
2016-09-10 23:57:02 -0400

    Improving migration to auto-init table as well, and improving dinner UI so it bolds the current day of week as well as menu item.

--- src/migrations/2016_09_08_200542_create_dinnermenu_table.php
+++ src/migrations/2016_09_08_200542_create_dinnermenu_table.php
@@ -2,21 +2,29 @@
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Cooglemirror\Dinner\Models\DinnerMenu;
 
 class CreateDinnermenuTable extends Migration {
 
 	/**
-	 * Run the migrations.
+	 * Run the migrations, populate the dinner menu
 	 *
 	 * @return void
 	 */
 	public function up()
 	{
 		Schema::create("dinner_menu", function(Blueprint $table) {
-		    $table->enum('day', ['sun', 'mon', 'tues', 'weds', 'thurs', 'fri'])->unique();
+		    $table->enum('day', ['sun', 'mon', 'tues', 'weds', 'thurs', 'fri', 'sat'])->unique();
 		    $table->string('menu');
 		    $table->timestamps();
 		});
+		
+		foreach(['sun', 'mon', 'tues', 'weds', 'thurs', 'fri', 'sat'] as $dayOfWeek) {
+		    $blankDay = new DinnerMenu();
+		    $blankDay->day = $dayOfWeek;
+		    $blankDay->menu = "?????";
+		    $blankDay->save();
+		}
 	}
 
 	/**
